aaa471e57ce0c958fe026e2f8ce44f3ab2dafbbb
Joshua <joshsmayhew@gmail.com>
2022-06-08 13:28:20 +0700

    commit tree bfd19589691f49aa3765294fc83409a172e54190

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+multiply;
+
+const random = Math.ceil(Math.random());
+
+Number.isInteger(random);
+
+
+
+const num3 = 36;
+const num4 = 8;
+const mod = num3 % num4;
+
+mod;
+
+
+const max = Math.max(Math.random(), 20);
+max;
+
